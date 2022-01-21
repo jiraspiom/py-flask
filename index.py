edba2fc164e0d1b6226e430697cb5fa046cedf0b
Jiraspiom <jiraspiom@yahoo.com.br>
2022-01-21 18:21:42 -0300

    projeto teste criado

---
+++ index.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__name)
+
+@app.route("/")
+def home():
+    return "Home page route"
+
+@app.route("/about")
+def about():
+    return "about page route"
+
+@app.route("/contact")
+def contact():
+    return "contatos route"
+
+@app.route('/api')
+def api():
+    with open('data.json', mode='r') as my_file:
+        text = my_file.read()
+        return text
+
